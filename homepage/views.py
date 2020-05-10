1bf8b3823cd33cbeabb2e001e6a1f729c1d606cd
iamsajjad <iamsajjad@mail.ru>
2020-05-10 05:28:15 +0300

    Initial commit

---
+++ homepage/views.py
@@ -0,0 +1,36 @@
+
+"""
+created by Sajjad @ https://github.com/iamsajjad
+"""
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from datetime import datetime
+from quickping import Quickping
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def runCheck(request):
+
+    sRange = request.POST['srange']
+    eRange = request.POST['erange']
+    ignore = request.POST['ignore']
+    
+    cams = Quickping(sRange, eRange, log=True, threads=512)
+    cams.active()
+    
+    ignore = ignore.split(' ')
+    print(ignore)
+    
+    cams.activeAddresses = [ip for ip in cams.activeAddresses if ip not in ignore]
+    cams.deactiveAddresses = [ip for ip in cams.deactiveAddresses if ip not in ignore]
+    
+    response = {
+            "start": cams.start,
+            "end": cams.end,
+            "active": cams.activeAddresses,
+            "deactive": cams.deactiveAddresses,
+    }
+    return render(request, 'homepage.html', response)
+
