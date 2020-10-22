2f514e41bdc6fb7290273cf3d244a534c72a466c
Sajjad alDalwachee <iamsajjad@mail.ru>
2020-10-22 06:10:44 +0300

    Update : add result info, threads input and update design
    - add result informations
        - number of addresses
        - time take to process
        - ignored addresses
    
    - add number of threads to form
    
    - set default values
    
    - update design

--- homepage/views.py
+++ homepage/views.py
@@ -1,33 +1,55 @@
 
-"""
+'''
 created by Sajjad @ https://github.com/iamsajjad
-"""
+'''
 
-from django.http import HttpResponseRedirect
+import time
+
 from django.shortcuts import render
-from datetime import datetime
 from quickping import Quickping
+
 
 def homepage(request):
     return render(request, 'homepage.html')
+
 
 def runCheck(request):
 
     sRange = request.POST['srange']
     eRange = request.POST['erange']
     ignore = request.POST['ignore']
+    threads = int(request.POST['threads'])
 
-    ignore = ignore.split('\n')
-    cams = Quickping(sRange, eRange, ignore=ignore, threads=512, log=True)
-    cams.active()
+    # remove repeated addresses
+    ignore = list(filter(None, ignore.replace('\r', '').split('\n')))
+    cleanAddresses = dict.fromkeys(ignore, 0)
+
+    ignore = []
+    for address in cleanAddresses.keys():
+        ignore.append(address.strip())
+
+    try:
+        stimer = time.time()
+        cams = Quickping(sRange, eRange, ignore=ignore, threads=threads, log=True)
+        cams.active()
+        etimer = time.time()
+    except Exception as e:
+        raise e
+
+    if len(cams.ignore) == 0:
+        cams.ignore = ['No Ignored Addresses']
+
+    # time take to process
+    timeToDone = time.strftime('%H:%M:%S', time.gmtime(etimer - stimer))
 
     response = {
-            "start": cams.start,
-            "end": cams.end,
-            "ignore": cams.ignore,
-            "active": cams.activeAddresses,
-            "deactive": cams.deactiveAddresses,
+        'start': cams.start,
+        'end': cams.end,
+        'ignore': cams.ignore,
+        'timeToDone': timeToDone,
+        'addresses': len(cams.addresses),
+        'active': cams.activeAddresses,
+        'deactive': cams.deactiveAddresses,
     }
 
     return render(request, 'homepage.html', response)
-
